3b8341fcb30d7eaf8a6ea9ced7ae80ec8fc7f8a2
Sunnie666 <260350726@qq.com>
2026-01-30 14:52:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+WECHAT_BIZ_LIST = [
+    ("知乎日报", "MzIzMjAxNzEzNA=="),
+    ("越疆机器人", "MzIyMDEyNjc3NA=="),  
+    ("节卡机器人", "MzU0NzEzNDc1OA=="), 
+    ("艾利特机器人", "MzU3NjAxMTM4NA=="), 
+    ("遨博机器人", "MzA4MDgyOTM5Mw=="), 
+    ("达明机器人", "MzI1NzQ5NzgwNQ=="), 
+    ("华沿机器人", "MzU2ODcxMTU2Nw=="), 
+    ("新松多可机器人", "MzUzNTg0MzQ2NA=="), 
+    ("ABB机器人", "MzA3ODA0OTMzMA=="), 
+    ("法奥机器人", "Mzg3MDY2OTY4Ng=="), 
+    ("珞石机器人", "MzIwMzA5NTU0Ng=="), 
+    ("UR机器人", "MzA4ODQyODQ2MQ"), 
+  
+]
